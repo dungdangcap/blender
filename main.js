5f7e81775f7eac91bdc18be3d858a9503c7e617b
turtle <dungvipvai205@gmail.com>
2022-11-14 12:35:11 +0700

    fisrt commit

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ changeButton.addEventListener(
         event.currentTarget.classList.toggle('dark')
     }
 )
-const changEbutton = document.querySelector('.all');
-changEbutton.addEventListener(
-    'click', function(event) {
-        event.currentTarget.classList.toggle('night')
-    }
-)
+// const changEbutton = document.querySelector('.all');
+// changEbutton.addEventListener(
+//     'click', function(event) {
+//         event.currentTarget.classList.toggle('night')
+//     }
+// )
 var dangeR = "Please click sun to change mode"
 alert(dangeR)
